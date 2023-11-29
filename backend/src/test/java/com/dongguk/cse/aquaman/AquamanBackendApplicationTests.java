8a65e196c6b18892af5cbb852b39b0ad8edd3bac
gsandoo <rogersddt@naver.com>
2023-11-29 09:25:15 +0900

    Init: 개발환경 구성

---
+++ backend/src/test/java/com/dongguk/cse/aquaman/AquamanBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dongguk.cse.aquaman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AquamanBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
