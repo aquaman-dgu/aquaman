db510061998e0ce2d1f1d0d91f99318ea240c540
gsandoo <rogersddt@naver.com>
2023-12-07 13:11:57 +0900

    Feat: 회원가입 및 jwt 로그인 구현

--- backend/src/main/java/com/dongguk/cse/aquaman/service/CustomUserDetailsService.java
+++ backend/src/main/java/com/dongguk/cse/aquaman/service/CustomUserDetailsService.java
@@ -7,7 +7,6 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -17,7 +16,6 @@ import java.util.Optional;
 public class CustomUserDetailsService implements UserDetailsService {
 
     private final MemberRepository memberRepository;
-    private final PasswordEncoder passwordEncoder;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -28,7 +26,6 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     // 해당하는 User 의 데이터가 존재한다면 UserDetails 객체로 만들어서 return
     private UserDetails createUserDetails(Member member) {
-//        member.setPassword(passwordEncoder.matches());
         return User.builder()
                 .username(member.getUsername())
                 .password(member.getPassword())
