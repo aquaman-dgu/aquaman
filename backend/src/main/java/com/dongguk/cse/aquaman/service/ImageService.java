95387cf9dff3a8b64233c5a25dbdd96e9a585650
gsandoo <rogersddt@naver.com>
2023-12-14 09:41:30 +0900

    Refact: 불필요한 코드 제거

--- backend/src/main/java/com/dongguk/cse/aquaman/service/ImageService.java
+++ backend/src/main/java/com/dongguk/cse/aquaman/service/ImageService.java
@@ -1,7 +1,6 @@
 package com.dongguk.cse.aquaman.service;
 
 import com.dongguk.cse.aquaman.dto.response.ImageResponseDTO;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
